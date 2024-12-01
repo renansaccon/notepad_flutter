2ec1e1942eeff7053a66e510b29557ed7b27fa1a
Renan Saccon <sacconrenan@gmail.com>
2024-12-01 12:44:09 -0300

    HomePage

---
+++ lib/themes/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData newTheme = ThemeData(
+  primaryColor: Colors.lightBlue,
+  secondaryHeaderColor: Colors.blue,
+  primaryColorDark: Colors.white,
+
+
+);
